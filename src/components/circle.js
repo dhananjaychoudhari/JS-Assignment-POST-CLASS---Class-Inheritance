0a1db57c9eee2469938adc7ef1baa9f3d7158e71
dhananjaychoudhari <76993420+dhananjaychoudhari@users.noreply.github.com>
2021-08-17 16:58:24 +0530

    Add files via upload

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,6 @@
-// import using require
-
-// declare class
-
-
-// export class using module.exports
+var Shape = require('./shape.js');
+class Circle extends Shape {
+    constructor() { super(); this.radius = 9; }
+    calculateArea() { return "Calculated area of circle"; }
+}
+module.exports = Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,6 @@
-// declare class
-
-// export class using module.exports
+class Shape {
+    constructor() { this.color = 'blue'; }
+    drawShape() { return "A shape is drawn"; }
+    calculateArea() { return "Area is calulated. "; }
+}
+module.exports = Shape;
